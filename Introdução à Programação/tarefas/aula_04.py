1fb04b954212f0e400fe081dc5e5ea14ca923dd8
VazFelipe <vaz.felipe@gmail.com>
2023-11-22 21:28:54 -0300

    adding aula 04

--- Introdução à Programação/tarefas/aula_04.py
+++ Introdução à Programação/tarefas/aula_04.py
@@ -15,8 +15,20 @@ print(indica_alcoolica, type(indica_alcoolica), sep=" , ")
 print("Exercício 2")
 almoco_favorito = "churrasco"
 almoco_preco = 79
-orçamento = 100
+orçamento = 70
 
 print("Meu orçamento é", orçamento)
 print("Meu almoço foi " + almoco_favorito + " e custou " + str(almoco_preco), sep=", ")
 print("O saldo após o almoço é", orçamento - almoco_preco)
+
+# Exercício 3
+
+print("Exercício 3")
+indica_amigo_dinheiro = False
+
+if orçamento >= almoco_preco:
+    print("Posso pagar o almoço")
+elif indica_amigo_dinheiro:
+    print("Pedir ajuda ao amigo")
+else:
+    print("Lavar a louça no restaurante")
