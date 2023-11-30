2019f1d6d3870d3d95e53a10f2c253f28f6ba162
Vaz <felipe.vaz@localiza.com>
2023-11-29 21:22:12 -0300

    adding solicita_numero.py

--- Introdução à Programação/tarefas/solicita_numero.py
+++ Introdução à Programação/tarefas/solicita_numero.py
@@ -2,6 +2,25 @@ def main():
     programa()
 
 def programa():
+    """
+    Função principal que solicita um número inteiro ao usuário e realiza algumas verificações.
+
+    A função solicita um número inteiro ao usuário e realiza as seguintes verificações:
+    - Se o número está entre zero e cem (inclusive);
+    - Se o número é par;
+    - Se o número é divisível por dois e três.
+
+    Se todas as verificações forem verdadeiras, imprime a mensagem "Número entre zero e 100, que é par e divisível por 2 e 3."
+    Se apenas a primeira e a segunda verificações forem verdadeiras, imprime a mensagem "Número entre zero e 100 e que é par."
+    Se apenas a primeira verificação for verdadeira, imprime a mensagem "Número entre zero e 100."
+    Caso contrário, não imprime nenhuma mensagem.
+
+    Raises:
+        ValueError: Se o usuário digitar algo diferente de um número inteiro.
+
+    Returns:
+        None
+    """
     
     entrada_dados = input("Digite um número inteiro: ")
 
@@ -18,6 +37,42 @@ def programa():
         except ValueError:
             print("\nVocê digitou algo diferente de um número inteiro. Tente novamente!")
             break
+
+def entre_zero_cem(numero):
+    """
+    Verifica se um número está entre zero e cem (inclusive).
+
+    Args:
+        numero (int): O número a ser verificado.
+
+    Returns:
+        bool: True se o número está entre zero e cem (inclusive), False caso contrário.
+    """
+    return 0 <= numero <= 100
+
+def numero_par(numero):
+    """
+    Verifica se um número é par.
+
+    Args:
+        numero (int): O número a ser verificado.
+
+    Returns:
+        bool: True se o número é par, False caso contrário.
+    """
+    return numero % 2 == 0
+
+def divisivel_dois_tres(numero):
+    """
+    Verifica se um número é divisível por dois e três.
+
+    Args:
+        numero (int): O número a ser verificado.
+
+    Returns:
+        bool: True se o número é divisível por dois e três, False caso contrário.
+    """
+    return numero % 2 == 0 and numero % 3 == 0
 
 def entre_zero_cem(entrada_dados):
     if 1 <= entrada_dados <= 100:
