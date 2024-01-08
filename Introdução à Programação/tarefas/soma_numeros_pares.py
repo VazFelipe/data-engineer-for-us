af9b1de313ef951f1051f4c56cc7ce33c626e2eb
vaz.felipe <vaz.felipe@gmail.com>
2024-01-08 20:25:43 -0300

    adicionando soma_numeros_pares.py

---
+++ Introdução à Programação/tarefas/soma_numeros_pares.py
@@ -0,0 +1,22 @@
+def operacao():
+     operacao = int(input('Digite a função desejada: '))
+     if (operacao == 1):
+        return operacao    
+
+
+def soma_pares(operacao):
+    
+    operacao = operacao()
+
+    if operacao == 1:
+        soma = 0
+
+        for numero in range(1,101):
+            if not numero % 2:
+                soma += numero
+        print(soma)
+    else:
+        print('esta operação ainda não foi criada')
+
+
+soma_pares(operacao)
