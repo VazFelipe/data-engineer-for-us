29f949297632d3aee3e6b6484262835f4b9c94a1
vaz.felipe <vaz.felipe@gmail.com>
2024-02-21 20:26:01 -0300

    adicionando javascript_05

---
+++ Introdução à Programação/javascript_05/script_2.js
@@ -0,0 +1,37 @@
+let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
+let valorSubtotal = document.getElementById("valor-subtotal");
+
+let subtotalInfo = {
+  quantidade: 1,
+  valor: 11.66,
+};
+
+quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+valorSubtotal.innerText = subtotalInfo.valor;
+
+
+const inputQtdMais = document.getElementById('quantidade-produto-01')
+const btnQtdMais = document.getElementById('btn-adicionar-produto-01')
+const inputQtdMenos = document.getElementById('quantidade-produto-01')
+const btnQtdMenos = document.getElementById('btn-subtrair-produto-01')
+
+function adicionarQuantidadeMais() {
+  inputQtdMais.value = Number(inputQtdMais.value) + 1
+}
+
+btnQtd.addEventListener('click', 
+
+function adicionarQuantidadeMais() {
+    inputQtdMais.value = Number(inputQtdMais.value) + 1
+  },
+
+function adicionarQuantidadeMenos() {
+    if (inputQtdMenos.value == 0) {
+        return inputQtdMenos.value = 0
+    } else {
+        inputQtdMenos.value = Number(inputQtdMenos.value) - 1
+    }
+}
+)
+
+ 
