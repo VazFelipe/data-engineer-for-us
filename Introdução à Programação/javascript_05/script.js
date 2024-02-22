5cf6c28504c218cc8ba193e24289e1d666624008
vaz.felipe <vaz.felipe@gmail.com>
2024-02-22 02:54:18 -0300

    adicionando javascript_05

--- Introdução à Programação/javascript_05/script.js
+++ Introdução à Programação/javascript_05/script.js
@@ -1,20 +1,29 @@
 let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
 let valorSubtotal = document.getElementById("valor-subtotal");
+let valorProduto = 11.66;
 
 let subtotalInfo = {
   quantidade: 1,
   valor: 11.66,
 };
 
-quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
-valorSubtotal.innerText = subtotalInfo.valor;
+function atualizarSubtotal() {
+    quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+    valorSubtotal.innerText = subtotalInfo.valor.toFixed(2);
+}
 
+atualizarSubtotal()
 
 const inputQtdMais = document.getElementById('quantidade-produto-01')
 const btnQtdMais = document.getElementById('btn-adicionar-produto-01')
 
 function adicionarQuantidadeMais() {
   inputQtdMais.value = Number(inputQtdMais.value) + 1
+
+  subtotalInfo.quantidade = subtotalInfo.quantidade + 1
+  subtotalInfo.valor = subtotalInfo.valor + valorProduto
+
+  atualizarSubtotal()
 }
 
 btnQtdMais.addEventListener('click', adicionarQuantidadeMais)
@@ -28,6 +37,11 @@ function adicionarQuantidadeMenos() {
     } else {
         inputQtdMenos.value = Number(inputQtdMenos.value) - 1
     }
+
+    subtotalInfo.quantidade = subtotalInfo.quantidade - 1
+    subtotalInfo.valor = subtotalInfo.valor - valorProduto
+
+    atualizarSubtotal()
   }
 
 btnQtdMenos.addEventListener('click', adicionarQuantidadeMenos)
