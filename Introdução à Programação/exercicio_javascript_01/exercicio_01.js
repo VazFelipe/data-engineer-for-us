ce614ebb89541204023c59400674281e9ed3aa94
vaz.felipe <vaz.felipe@gmail.com>
2024-03-06 21:19:42 -0300

    adicionando exercicio_01

---
+++ Introdução à Programação/exercicio_javascript_01/exercicio_01.js
@@ -0,0 +1,10 @@
+let numerosDaSorte = [37, 14, 26, 5, 94, 87]
+
+for (let i = 0; i < numerosDaSorte.length; i++) {
+  if (numerosDaSorte[i] < 50) {
+    console.log(numerosDaSorte[i], 'é menor que 50')
+  } else {
+    console.log(numerosDaSorte[i], 'é maior que 50')
+  };
+}
+
